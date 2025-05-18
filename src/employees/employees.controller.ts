2b9f8e78881b6dc924205e43fe07361d5ad5d966
fatahburiro66 <f18086863@gmail.com>
2025-05-18 09:02:02 +0500

    Throttler

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -11,7 +11,9 @@ import {
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma, Role } from 'generated/prisma';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -20,11 +22,14 @@ export class EmployeesController {
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
+
+  @SkipThrottle({ default: false })
   @Get()
   findAll(@Query('role') role?: Role) {
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({short:{ttl : 1000, limit: 1}})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
